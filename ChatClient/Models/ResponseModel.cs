84bb0608d68398bc680d7d2adbcd61273692cf77
jaewon lee <jwleetop5@naver.com>
2022-11-16 21:03:34 +0900

    끝.,?

---
+++ ChatClient/Models/ResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Models
+{
+    public class ResponseModel<T>
+    {
+
+        public  string code { get; set;}
+
+
+        public  string message { get; set;}
+
+        public T? options { get; set; }
+    }
+}
